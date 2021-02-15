65eaedc733b9821b8dfcf8e70637dd37f9fd5cd9
Jai Kumar <kjai1903@gmail.com>
2021-02-15 18:42:58 -0500

    Comp2139 Lab 2 Wednesday

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using comp2139_lab2_wednesday.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comp2139_lab2_wednesday.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculator tip)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Fifteen = tip.CalculateTip(0.15);
+                ViewBag.Twenty = tip.CalculateTip(0.20);
+                ViewBag.TwentyFive = tip.CalculateTip(0.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(tip);
+        }
+    }
+}
---
+++ Models/TipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace comp2139_lab2_wednesday.Models
+{
+    public class TipCalculator
+    {
+
+        [Required(ErrorMessage="Please enter a value for the cost of the Meal.")]
+        [Range(0.0,10000.0, ErrorMessage ="Please enter a value greater than zero.")]
+        public double? MealCost { get; set; }
+
+        public double CalculateTip(double percent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
